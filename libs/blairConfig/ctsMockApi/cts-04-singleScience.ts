fbed638aff3fac7a32d0223765c4d91bd7907f7d
a131381568 <a131381568@gmail.com>
2024-10-05 22:42:56 +0800

    feat: add cts mock data

---
+++ libs/blairConfig/ctsMockApi/cts-04-singleScience.ts
@@ -0,0 +1,14 @@
+export default {
+  data: {
+    getSinglePost: {
+      postid: 4840,
+      content:
+        "## 當月球運行至地球與太陽中間——\n\n> 月球的「影子」落在地球上，此時地球上該區域即可看到日食。\n\n<br />\n\n![356](https://i.imgur.com/qzM2qih.png)\n\n<br />\n\n在月球的「本影」處，可以看到日全食；而在「半影」處，則只能看到日偏食。\n\n<br />\n\n月球繞地球的軌道並非正圓形，而是橢圓形，月球距離地球的遠近並不一樣，當月球比較遠時，在地球上看起來比較小顆，此時月球若無法把太陽整顆遮住，就會看到日環食。\n\n<br />\n\n![361](https://i.imgur.com/iRVsPLp.png)\n\n<br />\n\n![362](https://i.imgur.com/k6Jyeyg.png)\n\n<br />\n\n![360](https://i.imgur.com/GeaBptd.png)\n\n<br />\n\n(依序為日全食、日偏食、日環食，取自[https://en.wikipedia.org/wiki/Solar_eclipse[(https://en.wikipedia.org/wiki/Solar_eclipse))\n\n<br />\n\n雖然就全地球而言，平均每18個月就會有某個地方發生日全食，但由於月球落在地球的本影區很小，就同一個地點來說，平均要300~400年才會看到一次日全食，每次日全食完全遮住的時間也只有持續不到幾分鐘而已。\n\n<br />\n\n從日食的開始到結束，以日全食來說共有5個重要階段：\n\n<br />\n\n![358](https://i.imgur.com/KvJXT1W.png)\n\n> 由上而下，分別是:\n\n- 初虧(太陽、月球第一次外切，太陽被咬第一口)\n- 食既(太陽、月球第一次內切，太陽完全被遮住)\n- 食甚(太陽、月球中心最接近的時刻)\n- 生光(太陽、月球第二次內切，太陽開始恢復)\n- 復圓(太陽、月球第二次外切，太陽完全恢復圓形)\n\n<br />\n\n從初虧至復圓，可以持續3小時左右，大部分時間可以慢慢地觀賞(不過食既-食甚-復圓只有幾分鐘，很短)，只是一定要注意使用安全的裝置來觀看太陽，例如太陽濾鏡、日食專用觀測眼鏡等，否則強烈的太陽光將造成視力永久性的損害!",
+      title: "日食",
+      categoryid: "phenomena",
+      updatetime: "1649174400000",
+      image: null,
+      __typename: "Artist",
+    },
+  },
+};
