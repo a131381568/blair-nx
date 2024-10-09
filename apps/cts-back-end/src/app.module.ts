32e7af2632f774ee835a234b163e97d3093adafb
a131381568 <a131381568@gmail.com>
2024-10-09 17:52:32 +0800

    feat: creat empty observatories modules

--- apps/cts-back-end/src/app.module.ts
+++ apps/cts-back-end/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AboutInfoModule } from './features/about-info/about-info.module';
 import { PrismaModule } from './features/shared/prisma.module';
 import { FacilitiesModule } from './features/facilities/facilities.module';
+import { ObservatoriesModule } from './features/observatories/observatories.module';
 
 @Module({
-	imports: [AboutInfoModule, PrismaModule, FacilitiesModule],
+	imports: [AboutInfoModule, PrismaModule, FacilitiesModule, ObservatoriesModule],
 	// controllers: [AppController],
 	// providers: [AppService],
 })
---
+++ apps/cts-back-end/src/features/observatories/observatories.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('observatories')
+export class ObservatoriesController {}
