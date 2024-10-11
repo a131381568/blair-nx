546346c33f733cdeaf209945255ef100222e86ea
a131381568 <a131381568@gmail.com>
2024-10-12 00:09:25 +0800

    feat: ctsb dev postCategories module

--- apps/cts-back-end/src/features/post-categories/post-categories-schemas.ts
+++ apps/cts-back-end/src/features/post-categories/post-categories-schemas.ts
@@ -3,10 +3,21 @@ import { z } from 'zod';
 const postCategoryBase = z.object({
 	postCategoryName: z.string().nullable(),
 	postCategoryId: z.string().nullable(),
+});
+
+const postCategoryWithNanoId = postCategoryBase.extend({
 	postCategoryNanoId: z.string().nullable(),
 });
 
-export const getPostCategoriesBaseSchema = z.array(postCategoryBase);
+const postCategoryDefaultSchema = z.object({
+	postCategoryName: z.string().nullable().default(''),
+	postCategoryId: z.string().nullable().default(''),
+});
 
-export type postCategoryBaseDto = z.infer<typeof postCategoryBase>;
-export type PostCategoriesBaseDto = z.infer<typeof getPostCategoriesBaseSchema>;
+export const getPostCategoriesBaseSchema = z.array(postCategoryWithNanoId);
+export const defaultPostCategoryData = postCategoryDefaultSchema.parse({});
+export const updatePostCategorySchema = postCategoryBase.strict();
+
+export type PostCategoryFitDto = z.infer<typeof postCategoryDefaultSchema>;
+export type PostCategoryDto = z.infer<typeof postCategoryWithNanoId>;
+export type PostCategoriesDto = z.infer<typeof getPostCategoriesBaseSchema>;
--- apps/cts-back-end/src/features/post-categories/post-categories.service.ts
+++ apps/cts-back-end/src/features/post-categories/post-categories.service.ts
@@ -3,18 +3,17 @@ import { pick, tryit } from 'radash';
 import { PrismaService } from '../shared/prisma.service';
 import { ApiResponse, createApiResponse } from '../../core/interceptors/api-response';
 import { PrismaErrorSchema } from '../shared/prisma-schemas';
-import { PostCategoriesBaseDto } from './post-categories-schemas';
+import { StrIdDto, StrIdSchema } from '../../common/dto/id.dto';
+import { PostCategoriesDto, PostCategoryFitDto, defaultPostCategoryData, updatePostCategorySchema } from './post-categories-schemas';
 
 @Injectable()
 export class PostCategoriesService {
 	constructor(private prisma: PrismaService) {}
 
-	async getPostCategories(): Promise<ApiResponse<PostCategoriesBaseDto>> {
+	async getPostCategories(): Promise<ApiResponse<PostCategoriesDto>> {
 		const [err, res] = await tryit(this.prisma.postCategories.findMany)({
 			orderBy: { postCategoryOrderId: 'asc' },
-			where: {
-				published: true,
-			},
+			where: {	published: true },
 		});
 
 		if (err && PrismaErrorSchema.safeParse(err).success)
@@ -25,5 +24,91 @@ export class PostCategoriesService {
 			return createApiResponse(false, [], 'list is empty');
 
 		return createApiResponse(true, res.map(item => pick(item, ['postCategoryId', 'postCategoryName', 'postCategoryNanoId'])));
+	}
+
+	async getPostCategory(id: StrIdDto): Promise<ApiResponse<PostCategoryFitDto>> {
+		const { success: zodSuccess, error: zodErr, data: safeId } = StrIdSchema.safeParse(id);
+
+		if (!zodSuccess)
+			return createApiResponse(false, defaultPostCategoryData, `Validation error: ${zodErr.errors[0].message}`);
+
+		const [err, res] = await tryit(this.prisma.postCategories.findFirst)({
+			where: { postCategoryNanoId: safeId, published: true },
+		});
+
+		if (err && PrismaErrorSchema.safeParse(err).success)
+			return createApiResponse(false, defaultPostCategoryData, 'Database error');
+		if (err)
+			return createApiResponse(false, defaultPostCategoryData, 'Unexpected error occurred');
+		if (!res)
+			return createApiResponse(false, defaultPostCategoryData, 'postCategoryId not found or no changes made');
+
+		return createApiResponse(true, pick(res, ['postCategoryId', 'postCategoryName']));
+	}
+
+	async updatePostCategory(
+		id: StrIdDto,
+		data: PostCategoryFitDto,
+	): Promise<ApiResponse<null>> {
+		const { success: idZodSuccess, error: idZodErr, data: safeId } = StrIdSchema.safeParse(id);
+		const { success: dataZodSuccess, error: dataZodErr, data: safeData } = updatePostCategorySchema.safeParse(data);
+
+		if (!dataZodSuccess || !idZodSuccess)
+			return createApiResponse(false, null, `Validation error: ${[dataZodErr?.errors[0].message, idZodErr?.errors[0].message].join('. ')}`);
+
+		const [err, res] = await tryit(this.prisma.postCategories.updateMany)({
+			where: { postCategoryNanoId: safeId, published: true },
+			data: safeData,
+		});
+
+		if (err && PrismaErrorSchema.safeParse(err).success)
+			return createApiResponse(false, null, 'Database error');
+		if (err)
+			return createApiResponse(false, null, 'Unexpected error occurred');
+		if (res && !res.count)
+			return createApiResponse(false, null, 'postCategoryId not found or no changes made');
+
+		return createApiResponse(true, null, 'Update success');
+	}
+
+	async createPostCategory(
+		data: PostCategoryFitDto,
+	): Promise<ApiResponse<null>> {
+		const { success: zodSuccess, error: zodErr, data: safeData } = updatePostCategorySchema.safeParse(data);
+
+		if (!zodSuccess)
+			return createApiResponse(false, null, `Validation error: ${zodErr.errors[0].message}`);
+
+		const [err] = await tryit(this.prisma.postCategories.create)({
+			data: { ...safeData, published: true },
+		});
+
+		if (err && PrismaErrorSchema.safeParse(err).success)
+			return createApiResponse(false, null, 'Database error');
+		if (err)
+			return createApiResponse(false, null, 'Unexpected error occurred');
+
+		return createApiResponse(true, null, 'Create success');
+	}
+
+	async deletePostCategory(id: StrIdDto): Promise<ApiResponse<null>> {
+		const { success: zodSuccess, error: zodErr, data: safeId } = StrIdSchema.safeParse(id);
+
+		if (!zodSuccess)
+			return createApiResponse(false, null, `Validation error: ${zodErr.errors[0].message}`);
+
+		const [err, res] = await tryit(this.prisma.postCategories.updateMany)({
+			where: { postCategoryNanoId: safeId, published: true },
+			data: { published: false },
+		});
+
+		if (err && PrismaErrorSchema.safeParse(err).success)
+			return createApiResponse(false, null, 'Database error');
+		if (err)
+			return createApiResponse(false, null, 'Unexpected error occurred');
+		if (res && !res.count)
+			return createApiResponse(false, null, 'postCategoryId not found or no changes made');
+
+		return createApiResponse(true, null, 'Delete success');
 	}
 }
