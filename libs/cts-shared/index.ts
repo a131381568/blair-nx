6e2a3d14225ceb5eb96883ab2869ee9691589ac4
a131381568 <a131381568@gmail.com>
2024-10-25 00:47:47 +0800

    refactor: ctsb change stargazing api method by ts-rest contracts

--- libs/cts-shared/index.ts
+++ libs/cts-shared/index.ts
@@ -28,3 +28,4 @@ export * from './contracts/file.contracts';
 export * from './contracts/observatories.contracts';
 export * from './contracts/page-info.contracts';
 export * from './contracts/post-categories.contracts';
+export * from './contracts/stargazing.contracts';
