32351ef99a689aff843f12408ca2fe185c249084
a131381568 <a131381568@gmail.com>
2024-10-30 08:26:55 +0800

    feat: ctsf add helper - customRoute

---
+++ apps/cts-front-end/src/helper/customCtsRoute.ts
@@ -0,0 +1,7 @@
+import type { Router } from 'vue-router';
+
+export const hrefLoginPage = () => (window.location.href = '/login');
+
+export const linkBoardPage = (router: Router) => router.push('/board');
+
+export const linkNotFoundPage = (router: Router) => router.push('/notfound');
